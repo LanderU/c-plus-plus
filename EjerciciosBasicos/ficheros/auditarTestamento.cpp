0a91644870b61573e6b7147a8336a149fa9cc6cd
Lander Usategui San Juan <lander.usategui@gmail.com>
2016-08-14 20:11:06 +0200

    Added auditarTestamento and necessary files

---
+++ EjerciciosBasicos/ficheros/auditarTestamento.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+// Firma de las funciones
+
+void AbrirFicheros(ifstream&, ofstream&);
+void Conteo(char, int&, int&, int&, int&, int&, int&);
+void ImprimirTabla(ofstream& tabla, int, int, int, int, int, int);
+
+
+int main() {
+
+	ifstream texto;
+	ofstream tabla;
+	char caracter;
+
+	// Contadores
+	int mayusculas = 0;
+	int minusculas = 0;
+	int espacios = 0;
+	int contCaracteres = 0;
+	int signosPuntuacion = 0; // '.', '!', '?'
+	int resto = 0;
+
+	AbrirFicheros(texto, tabla);
+
+	if(!texto || !tabla){
+
+		cout << "Alguno de los archivos o ambos no se han podido abrir." << endl;
+
+		return 1;
+
+	}else {
+
+		texto.get(caracter);
+
+		do {
+
+			// Efectuamos la llamada a la función para hacer el conteo
+			Conteo(caracter, mayusculas, minusculas, espacios, contCaracteres, signosPuntuacion, resto);
+
+			texto.get(caracter);
+
+		}while(texto); // Mientras sigamos teniendo datos que procesar continuamos
+
+		// Llamamos a la función para guardar los datos en el fichero
+		ImprimirTabla(tabla, mayusculas, minusculas, espacios, contCaracteres, signosPuntuacion, resto);
+
+		// Cerramos los flujos
+		texto.close();
+		tabla.close();
+		return 0;
+	}// end if
+
+}// end main
+
+// Funciones
+
+void AbrirFicheros(ifstream& texto, ofstream& tabla){
+
+	string archivoEntrada;
+	string archivoSalida;
+
+	cout << "Escriba el nombre del archivo: ";
+	cin >> archivoEntrada;
+
+	texto.open(archivoEntrada.c_str());
+
+	cout << "Escriba el nombre del fichero de salida: ";
+	cin >> archivoSalida;
+
+	tabla.open(archivoSalida.c_str());
+
+	// Introducimos un encabezado.
+
+	tabla << "Análisis de caracteres en el archivo de salida " << archivoSalida << endl;
+
+}// end AbrirFicheros
+
+void Conteo(char caracter, int& mayusculas, int& minusculas, int& espacios, int& contCaracteres, int& signosPuntuacion, int& resto){
+
+	if(isupper (caracter)){
+		mayusculas++;
+	}else if(islower(caracter)){
+		minusculas++;
+	}else if(caracter == ' '){
+		espacios++;
+	}else if(isdigit(caracter)){
+		contCaracteres++;
+	}else if((caracter == '.') || (caracter == '?') || (caracter == '!')){
+		signosPuntuacion++;
+	}else{
+		resto++;
+	}// end if
+
+}// end Conteo
+
+void ImprimirTabla(ofstream& tabla, int mayusculas, int minusculas, int espacios, int contCaracteres, int signosPuntuacion, int resto){
+
+	const int MULTIPLICADOR = 100;
+
+	float total = mayusculas + minusculas + espacios + contCaracteres + signosPuntuacion +resto;
+
+	// Escribimos los resultados
+	tabla << fixed << setprecision(3) << "Porcentaje de mayúsculas: " << (mayusculas/total) * MULTIPLICADOR << endl;
+	tabla << fixed << setprecision(3) << "Porcentaje de minusculas: " << (minusculas/total) * MULTIPLICADOR << endl;
+	tabla << fixed << setprecision(3) << "Porcentaje de espacios en blanco: " << (espacios/total) * MULTIPLICADOR << endl;
+	tabla << fixed << setprecision(3) << "Porcentaje de caracteres: " << (contCaracteres/total) * MULTIPLICADOR << endl;
+	tabla << fixed << setprecision(3) << "Porcentaje de signos de puntuación: " << (signosPuntuacion/total) * MULTIPLICADOR << endl;
+	tabla << fixed << setprecision(3) << "Porcentaje del resto de caracteres: " << (resto/total) * MULTIPLICADOR << endl;
+
+}// end ImprimirTabla
